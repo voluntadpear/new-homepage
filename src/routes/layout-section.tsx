74e04b45439917c9969d5e7fe4c7fe7dce4e2db0
Guillermo Peralta Scura <gperaltascura@gmail.com>
2022-10-16 19:33:13 -0300

    Less hacky document title approach in layout

--- src/routes/layout-section.tsx
+++ src/routes/layout-section.tsx
@@ -1,25 +1,27 @@
 import { component$, Slot } from "@builder.io/qwik";
-import { DocumentHead } from "@builder.io/qwik-city";
+import { DocumentHead, useDocumentHead } from "@builder.io/qwik-city";
 import { DesktopMenu, MobileMenu } from "~/components/header";
 import { GitHub, LinkedIn, Twitter } from "~/components/icons/qwik";
 
-export let pageTitle: string | null;
+export const titleSuffix = " | Guille";
 
 export default component$(() => {
+  const head = useDocumentHead();
+
   return (
     <>
       <header class="bg-my-orange-light">
         <div class="lg:max-w-5xl mx-auto">
           <MobileMenu />
           <DesktopMenu />
-          <h1 class="text-3xl text-my-blue mt-6 pb-6 md:ml-8 md:mt-24 lg:ml-0">
-            {pageTitle}
+          <h1 class="text-3xl md:text-4xl text-my-blue mt-7 pb-6 ml-4 md:ml-8 md:mt-24 lg:ml-0">
+            {head.title.replace(titleSuffix, "")}
           </h1>
         </div>
       </header>
       <Slot />
       <footer class="bg-my-orange-light">
-        <div class="pt-8 pb-12 pl-4 md:pl-9 lg:pl-0 lg:pt-20 lg:pb-28 lg:max-w-5xl lg:mx-auto">
+        <div class="pt-8 pb-12 pl-4 md:pl-9 md:pt-16 lg:pl-0 lg:pt-20 lg:pb-28 lg:max-w-5xl lg:mx-auto">
           <p class="text-my-blue underline md:text-2xl">
             <a href="/">Go back home</a>
           </p>
@@ -35,9 +37,7 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = ({ head }) => {
-  pageTitle = head.title;
-
   return {
-    title: `${head.title} | Guille`,
+    title: `${head.title}${titleSuffix}`,
   };
 };
